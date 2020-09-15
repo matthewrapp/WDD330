207c008a3f5acf27182a73e5c211b880547e2a78
matthewrapp <mattrapp25@gmail.com>
2020-09-15 16:28:22 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-var ol = document.getElementById("orderedList");
-
 var weeks = [
     "Week 1",
-    "Week 2"
+    "Week 2",
 ]
 
-function newListItem() {
-    $("#orderedList").append(weeks[0]);
-}
+document.getElementById("orderedList").innerHTML = weeks[0];
