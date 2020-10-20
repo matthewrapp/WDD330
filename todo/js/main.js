52310e4581a2b262957bb6fd005e3c2ad7e302b1
matthewrapp <mattrapp25@gmail.com>
2020-10-20 01:04:06 -0600

    Todo App (Oct 20)

--- todo/js/main.js
+++ todo/js/main.js
@@ -1,18 +1,45 @@
 // import named export
 import { qs } from './utilities.js';
 // import default export
-import Todo, { saveTodos } from './todo.js';
+import Todo, { } from './todo.js';
 
 const myTodo = new Todo('#todoList', 'todos');
 
-// qs("#addButton").addEventListener("click", myTodo.addNewTodo);
-qs("#addButton").addEventListener("click", () => {
-    let todoInput = document.getElementById("todoInput").value;
-    myTodo.addNewTodo(todoInput);
-});
 
-window.onload = () => {
+// qs("#addButton").addEventListener("click", myTodo.addNewTodo);
+// qs("#addButton").addEventListener("click", () => {
+//     let todoInput = qs("#todoInput").value;
+//     myTodo.addNewTodo(todoInput);
+// });
+
+window.onload = (event) => {
     myTodo.loadTodos();
 }
 
+document.addEventListener("click", (e) => {
+    if (e.target) {
+        if (e.target.className == "remove") {
+            let idToDelete = e.target.getAttribute("data-todoItemId");
+            console.log(`Removing item with id: ${idToDelete}`);
+            let elementToDelete = e.target.parentElement;
 
+            myTodo.removeTodo(idToDelete, elementToDelete);
+        } else if (e.target.id == "addButton") {
+            let todoInput = qs("#todoInput").value;
+            console.log(`Adding todo item ${todoInput}`);
+
+            myTodo.addNewTodo(todoInput);
+            qs("#todoInput").value = "";
+        }
+    }
+})
+
+// qs(".remove").addEventListener("click", () => {
+//     console.log(newTodo.id);
+// });
+
+// qs(".remove").addEventListener("click", function(e) {
+//     if (e.target.className == "remove") {
+//         alert('Clicked');
+//     }
+// });
--- todo/js/todo.js
+++ todo/js/todo.js
@@ -19,50 +19,125 @@
 
 // import named export from 'utilities.js'   
 import { qs, saveToLocalStorage, getFromLocalStorage } from './utilities.js';
-const todos = [];
+// const todos = [];
 export function saveTodos(key, data) {
     saveToLocalStorage(key, data);
 }
+
+class todoItem {
+    constructor(text) {
+        this.text = text;
+        this.id = new Date();
+        this.completed = false;
+    }
+
+    complete() {
+        this.completed = true;
+    }
+}
+
 
 class Todo {
     // constructor special func that will automatically get called when you do 'new' (const myTody) = new Todo();
     constructor(parentId, key) {
          this.listElement = qs(parentId);
          this.key = key;
+         this.todoItems = [];
     }
 
     // funciton in a class is a 'method' & you do NOT add function keyword
     addNewTodo(text) {
-        const newTodo = {
-            id: new Date(),
-            text: text,
-            completed: false,
-        }
+        const newTodo = new todoItem(text);
+        this.todoItems.push(newTodo);
 
+        // create variables for within the buttons (icons or text)
+        let removeSVG = "Remove"// `<i class="fa fa-trash"></i>`;
+        let completeSVG = "Check" // `<i class="fa fa-check"></i>`;
+        // create buttons div
+        let buttons = document.createElement('div');
+        buttons.classList.add("buttons");
+        // create remove task button
+        let removeBtn = document.createElement('button');
+        removeBtn.classList.add('remove');
+        removeBtn.innerHTML = removeSVG;
+        removeBtn.setAttribute("data-todoItemId", newTodo.id);
+        // create complete task button
+        let completeBtn = document.createElement('button');
+        completeBtn.classList.add('complete');
+        completeBtn.innerHTML = completeSVG;
+        // append each button into the buttons div
+        buttons.appendChild(completeBtn);
+        buttons.appendChild(removeBtn);
+
+        // create list item (li)
         let todoList = document.getElementById("todoList");
-        todoList.innerHTML += '<li>' + " " + newTodo.text + " " + newTodo.id + '</li>';
+        todoList.innerHTML += '<li class="task">' + " " + text + buttons.innerHTML + '</li>';
 
-        todos.push(newTodo);
-        saveTodos(this.key, todos);
-    }
-
-    completeTodo(todo) {
+        // save to local storage
+        // todos.push(newTodo);
+        saveTodos(this.key, this.todoItems);
 
     }
 
-    removeTodo(todo) {
-
+    getTodoItem(id) {
+        return this.todoItems.find(item => item.id == id);
     }
 
+    // remove todo items and put into another array
+    removeTodo(id, elementToDelete) {
+        let deletedItemsList = [];
+        this.todoItems.forEach(item => {
+            if (item.id != id) {
+                deletedItemsList.push(item);
+            }
+        })
+
+        elementToDelete.remove();
+        this.todoItems = deletedItemsList;
+
+        // reset local storage
+        saveTodos(this.key, this.todoItems);
+    }
+
+    // load todos
     loadTodos() {
         let jsonList = getFromLocalStorage(this.key);
-        console.log(jsonList);
-        let todoList = JSON.parse(jsonList || "[]");
-        todoList.forEach(item => {
-            this.listElement.innerHTML += '<li>' + " " + item.text + " " + item.id + '</li>';
-        })
+        let items = JSON.parse(jsonList || "[]");
+        this.listElement.childNodes.forEach(child => child.remove());
+        this.todoItems = [];
+
+        if (items != null) {
+            items.forEach(item => {
+            this.todoItems.push(item);
+
+            // create variables for within the buttons (icons or text)
+            let removeSVG = "Remove"// `<i class="fa fa-trash"></i>`;
+            let completeSVG = "Check" // `<i class="fa fa-check"></i>`;
+            // create buttons div
+            let buttons = document.createElement('div');
+            buttons.classList.add("buttons");
+            // create remove task button
+            let removeBtn = document.createElement('button');
+            removeBtn.classList.add('remove');
+            removeBtn.innerHTML = removeSVG;
+            removeBtn.setAttribute("data-todoItemId", item.id);
+            // create complete task button
+            let completeBtn = document.createElement('button');
+            completeBtn.classList.add('complete');
+            completeBtn.innerHTML = completeSVG;
+            // append each button into the buttons div
+            buttons.appendChild(completeBtn);
+            buttons.appendChild(removeBtn);
+
+            // create list item (li)
+            let todoList = document.getElementById("todoList");
+            todoList.innerHTML += '<li class="task">' + " " + item.text + buttons.innerHTML + '</li>';
+
+            }) 
+        }
     }
 
 }
+
 
 export default Todo;
