c2ed4337ba444f7e67be1ee6836c93228f4d55b4
matthewrapp <mattrapp25@gmail.com>
2020-12-11 00:55:47 -0700

    commit

---
+++ project2/js/firebaseApp.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCtDyNo_rmZZn-F9TNkffb0L_aVQBoC5gs",
+    authDomain: "links-webapp.firebaseapp.com",
+    projectId: "links-webapp",
+    storageBucket: "links-webapp.appspot.com",
+    messagingSenderId: "40927563122",
+    appId: "1:40927563122:web:29e862ea43b5a9dd2521eb",
+    measurementId: "G-MS8SQCGGHV",
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
+//   firebase.analytics();
